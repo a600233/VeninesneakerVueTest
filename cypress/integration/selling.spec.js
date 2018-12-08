f236e1c021641980ad469330293ddf60f3df4f0b
WujiugeYin <ywjg_99@126.com>
2018-12-08 14:59:29 +0000

    selling page e2e test

---
+++ cypress/integration/selling.spec.js
@@ -0,0 +1,42 @@
+
+describe('Manage Selling page', () => {
+  beforeEach(() => {
+    // Delete all selling in the API's datastore
+    cy.request('https://veninesnekaer.herokuapp.com/selling/')
+      .its('body')
+      .then((selling) => {
+        selling.forEach((element) => {
+          cy.request('DELETE',
+            'https://veninesnekaer.herokuapp.com/selling/' + element._id)
+        })
+      })
+    // Populate API's datastore
+    cy.fixture('selling')
+      .then((selling) => {
+        selling.forEach((selling) => {
+          cy.request('POST',
+            'https://veninesnekaer.herokuapp.com/selling/', selling)
+        })
+      })
+    cy.visit('/')
+    // Click Manage Donations navbar link
+    cy.get('.navbar-nav:nth-child(1)')
+      .find('.nav-item:nth-child(2)').click()
+  })
+
+  it('allows a selling to be deleted', () => {
+    cy.get('tbody').find('tr').should('have.length', 6)
+    // Click trash/delete link of 3rd selling in list
+    cy.get('tbody').find('tr:nth-child(3)').find('td:nth-child(12)').click()
+    // Click confirmation button
+    cy.get('button').contains('Delete').click()
+    cy.get('tbody').find('tr').should('have.length', 5)
+  })
+
+  it('allows a set of selling info to increase amount', () => {
+    // Click + symbol of 2nd selling in list
+    cy.get('tbody').find('tr').should('have.length', 6)
+    cy.get('tbody').find('tr:nth-child(5)').find('td:nth-child(10)').click()
+    cy.get('tbody').find('tr:nth-child(5)').find('td:nth-child(9)').should('contain', 2)
+  })
+})
--- src/components/Selling.vue
+++ src/components/Selling.vue
@@ -3,7 +3,7 @@
     <h3 class="vue-title"><i class="fa fa-list" style="padding: 3px"></i>{{messagetitle}}</h3>
     <div id="app1">
       <v-client-table :columns="columns" :data="selling" :options="options">
-        <a slot="upamount" slot-scope="props" class="fa fa-thumbs-up fa-2x" @click="upamountSelling(props.row._id)"></a>
+        <a slot="upamount" slot-scope="props" class="fa fa-plus fa-2x" @click="upamountSelling(props.row._id)"></a>
         <a slot="remove" slot-scope="props" class="fa fa-trash-o fa-2x" @click="deleteSelling(props.row._id)"></a>
         <a slot="edit" slot-scope="props" class="fa fa-edit fa-2x" @click="editSelling(props.row._id)"></a>
       </v-client-table>
